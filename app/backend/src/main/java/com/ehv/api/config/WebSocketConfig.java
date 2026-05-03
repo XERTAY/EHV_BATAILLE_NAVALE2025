7b00ea924fa90c3c061cb6eaa1db1e72973a68b0
BEILBORNCOMBE-Elouann <elouannbeilborn@gmail.com>
2026-05-03 15:14:23 +0200

    ajoute du websocket et doc explicatif de l'utilisation

---
+++ app/backend/src/main/java/com/ehv/api/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.ehv.api.config;
+
+import java.util.Objects;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final @NonNull GameWebSocketHandler gameWebSocketHandler;
+
+    public WebSocketConfig(GameWebSocketHandler gameWebSocketHandler) {
+        this.gameWebSocketHandler = Objects.requireNonNull(gameWebSocketHandler);
+    }
+
+    @Override
+    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+        registry.addHandler(gameWebSocketHandler, "/ws/game")
+                .setAllowedOrigins("http://localhost:5173")
+                .addInterceptors(new HttpSessionHandshakeInterceptor());
+    }
+}
